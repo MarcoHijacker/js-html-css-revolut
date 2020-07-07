fe9b32e4098a1667b749d13f12298dd15d6249d7
Marco <marco.longo@hotmail.it>
2020-07-07 17:59:16 +0200

    Versione quasi finale, manca menu XS.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 $(document).ready(function () {
   $('.menu-item > a').mouseover(function () { // Mouse over sull'a di menu-item
     $('.dropdown-menu').hide(); // Nascondo tutti i menu a tendina
+    $('.menu-item > a').removeClass('active'); // Tolgo il colore nero di selezione delle voci di menu dopo che il mouse ha selezionato un'altra voce della navbar
     $(this).addClass('active'); // La classe active aggiunge il colore nero alla voce del menu attiva che sta mostrando il menu a tendina
     $(this).next().show(); // Mostro il div successivo al menu-item > a attivo
   });
