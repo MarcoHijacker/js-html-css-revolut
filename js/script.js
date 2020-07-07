de48953183467ffcdabb6e3b2a26e2721293c043
Marco <marco.longo@hotmail.it>
2020-07-07 17:15:38 +0200

    XL + jQ menu, XS fix.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@
 $(document).ready(function () {
   $('.menu-item > a').mouseover(function () { // Mouse over sull'a di menu-item
     $('.dropdown-menu').hide(); // Nascondo tutti i menu a tendina
+    $(this).addClass('active'); // La classe active aggiunge il colore nero alla voce del menu attiva che sta mostrando il menu a tendina
     $(this).next().show(); // Mostro il div successivo al menu-item > a attivo
   });
-  $('.main-menu').mouseleave(function () { // Faccio in modo che il menu attivo sparisca appena esco dal div contenente la navbar
-    $('.dropdown-menu').hide();
+  $('.main-menu').mouseleave(function () {
+    $('.dropdown-menu').hide(); // Faccio in modo che il menu attivo sparisca appena esco dal div contenente la navbar
+    $('.menu-item > a').removeClass('active'); // Tolgo il colore nero di selezione delle voci di menu dopo che il mouse ha abbandonato la navbar
   });
 });
