764ece945e4ed7578e8df86558e4070a593e8c11
Marco <marco.longo@hotmail.it>
2020-07-07 14:20:13 +0200

    Vieport Desktop + Menu in JS completi.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // Revolut Template JS + jQuery
+
+$(document).ready(function () {
+  $('.menu-item > a').mouseover(function () { // Mouse over sull'a di menu-item
+    $('.dropdown-menu').hide(); // Nascondo tutti i menu a tendina
+    $(this).next().show(); // Mostro il div successivo al menu-item > a attivo
+  });
+  $('.main-menu').mouseleave(function () { // Faccio in modo che il menu attivo sparisca appena esco dal div contenente la navbar
+    $('.dropdown-menu').hide();
+  });
+});
