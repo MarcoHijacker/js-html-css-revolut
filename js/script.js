9d3d1f16db70d6716ae352893c7ed77b39505547
Marco <marco.longo@hotmail.it>
2020-07-08 16:18:04 +0200

    Versione definitiva.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 // Revolut Template JS + jQuery
 
 $(document).ready(function () {
+
   $('.menu-item > a').mouseover(function () { // Mouse over sull'a di menu-item
     $('.dropdown-menu').hide(); // Nascondo tutti i menu a tendina
     $('.menu-item > a').removeClass('active'); // Tolgo il colore nero di selezione delle voci di menu dopo che il mouse ha selezionato un'altra voce della navbar
     $(this).addClass('active'); // La classe active aggiunge il colore nero alla voce del menu attiva che sta mostrando il menu a tendina
     $(this).next().show(); // Mostro il div successivo al menu-item > a attivo
   });
+
   $('.main-menu').mouseleave(function () {
     $('.dropdown-menu').hide(); // Faccio in modo che il menu attivo sparisca appena esco dal div contenente la navbar
     $('.menu-item > a').removeClass('active'); // Tolgo il colore nero di selezione delle voci di menu dopo che il mouse ha abbandonato la navbar
@@ -23,6 +25,11 @@ $(document).ready(function () {
   });
 
   $('.mobile-item span').click(function () { // Al click su una voce di menu mobile
+    // Inizializzo la situazione di partenza per far sì che dopo un click su una voce le altre si chiudano e deselezionino
+    $('.mobile-item span').removeClass('active'); // Tolgo la classe che rende il colore nero sulla voce precedentemente attiva (in realtà la tolgo a tutte)
+    $('.mobile-item > ul').hide(); // Nascondo tutti i menu a tendina
+    $('.mobile-item span > i').removeClass('fa-chevron-up active').addClass('fa-chevron-down'); // Ripristino l'immagine con la freccia indicatrice delle varie voci dei menu
+
     $(this).toggleClass('active'); // Aggiungo colore nero in classe active allo span cliccato
     $(this).next().slideToggle(); // Faccio andare in display block l'ul successivo allo span cliccato
     if($(this).children().hasClass('fa-chevron-down')) { // Qui sistemo la logica della freccia che indica se il menu è aperto o chiuso
@@ -33,4 +40,5 @@ $(document).ready(function () {
       $(this).children().addClass('fa-chevron-down'); // E rimetto la freccia giù inattiva
     }
   });
+  
 });
