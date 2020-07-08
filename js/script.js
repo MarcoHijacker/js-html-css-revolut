e07eddd50b990af05728fa5972fa475e21e40881
Marco <marco.longo@hotmail.it>
2020-07-08 13:01:14 +0200

    Inserito menu hamburger mobile con jQ funzionante.

--- js/script.js
+++ js/script.js
@@ -11,4 +11,26 @@ $(document).ready(function () {
     $('.dropdown-menu').hide(); // Faccio in modo che il menu attivo sparisca appena esco dal div contenente la navbar
     $('.menu-item > a').removeClass('active'); // Tolgo il colore nero di selezione delle voci di menu dopo che il mouse ha abbandonato la navbar
   });
+
+  $('.hamburger-menu').click(function () { // Al click sul menu hamburger
+    $('header').hide(); // nascondo l'header con tutti i contenuti in viewport
+    $('#mobile-menu').show(); // E mostro una section#mobile-menu con il menu mobile
+  });
+
+  $('.mobile-right a i').click(function () { // Una volta aperto il menu mobile, avrò una (X) in alto a dx, al click su di essa
+    $('#mobile-menu').hide(); // Nascondo menu mobile
+    $('header').show(); // Mostro header principale
+  });
+
+  $('.mobile-item span').click(function () { // Al click su una voce di menu mobile
+    $(this).toggleClass('active'); // Aggiungo colore nero in classe active allo span cliccato
+    $(this).next().slideToggle(); // Faccio andare in display block l'ul successivo allo span cliccato
+    if($(this).children().hasClass('fa-chevron-down')) { // Qui sistemo la logica della freccia che indica se il menu è aperto o chiuso
+      $(this).children().removeClass('fa-chevron-down'); // Se sul click la freccia è giù tolgo questa
+      $(this).children().addClass('fa-chevron-up active'); // E la rimpiazzo con una freccia in su, colore nero
+    } else {
+      $(this).children().removeClass('fa-chevron-up active'); // Altrimenti faccio l'inverso di sopra, tolgo freccia su attiva
+      $(this).children().addClass('fa-chevron-down'); // E rimetto la freccia giù inattiva
+    }
+  });
 });
